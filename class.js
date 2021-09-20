d05456921f90d4138e9dd6df3ecd45ae9488a0e1
Sadat Hossen Himel <87661192+opeltanvir@users.noreply.github.com>
2021-09-21 00:37:20 +0600

    class

---
+++ class.js
@@ -0,0 +1,15 @@
+class Student{
+    constructor(sId, sName){
+        this.id = sId;
+        this.name = sName;
+        this.school = "Kolimunnesa School "
+
+    }
+}
+
+
+const student1= new Student(12, "shuvo");
+const student2= new Student(22 , "mahiya");
+const student3 = new Student(23, "Bappi");
+
+console.log(student3);
