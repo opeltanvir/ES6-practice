595a61d843f1ea96c2a66d0ab916f9e628328226
Sadat Hossen Himel <87661192+opeltanvir@users.noreply.github.com>
2021-09-20 23:29:12 +0600

    template

---
+++ template.js
@@ -0,0 +1,12 @@
+const firstName = "justin"
+const lastName= "TimberLake"
+const fullName = firstName + " " + lastName + " is a good boy";
+const fullName2 = `&{firstName} ${lastName} is a good boy.`;
+const multiLine = "I love you"
+
+const multiLine2 =`I love you
+I miss you
+no. I don't need you
+Baily road e dorkar nai.`
+
+console.log(multiLine2);
