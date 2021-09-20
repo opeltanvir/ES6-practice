84671c557924fbc84adc518210b485e9f1aad6bc
Sadat Hossen Himel <87661192+opeltanvir@users.noreply.github.com>
2021-09-20 22:35:35 +0600

    upload

---
+++ letcons.js
@@ -0,0 +1,17 @@
+const hubby = "Elish Kanchon";
+console.log(hubby);
+
+const numbers = [12, 45];
+numbers [1] = 88;
+numbers.push(12);
+const nayok = {name:"sakib khan",phone:4578};
+
+// console.log(numbers);
+
+let patientNumber = "Rahim Chacha"
+console.log(patientNumber);
+let sum = 0;
+for(i = 0; i< 10 ;i++){
+    sum = sum + i;
+}
+console.log(i);
