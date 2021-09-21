7dd692497baa259c3732a8c153f9fe776e9706c8
Sadat Hossen Himel <87661192+opeltanvir@users.noreply.github.com>
2021-09-21 13:21:00 +0600

    update

---
+++ destructure.js
@@ -0,0 +1,14 @@
+const person = {name: 'jack Willam', age: 17, job:'facebook',gfName: 'Ema Watsone', address:'Kohu Khata', phone:01674937395,friends: ['Tom hancks','tom cruise','tom yarn'] }
+
+const { phone ,gfName ,address,salay } = person;
+
+
+// const gfName = person.gfName;
+// console.log(gfName,phone , salay, address);
+
+
+
+const friends=['sakib khan', 'arman khan','salman khan']
+const [chotoFriend, nextFriend ,...olderF] = friends;
+console.log(olderF);
+
